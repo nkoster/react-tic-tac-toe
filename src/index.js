6919dbe3edbe4c2b969971d14d758adcb4c6ffa5
Niels Koster <n.koster@portavita.eu>
2018-01-12 10:22:50 +0100

    Exit if winner

--- src/index.js
+++ src/index.js
@@ -61,6 +61,9 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
